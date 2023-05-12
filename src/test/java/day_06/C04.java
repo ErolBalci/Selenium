7464e5aa439fe3fd34d456543bdc28eb205fe88a
Erol Balci <erolbalci3706@gmail.com>
2023-05-12 19:44:27 +0300

    Emekli Olma Kontrolü

---
+++ src/test/java/day_06/C04.java
@@ -0,0 +1,40 @@
+package day_06;
+
+import java.util.Scanner;
+
+public class C04 {
+    public static void main(String[] args) {
+
+        // Soru 3- Kullanicidan yasini isteyin,
+        //         65 yas ve uzeri ise ”Emekli olabilirsin” yazdirin,
+        //         yoksa emekli olmasi icin calismasi gereken yil sayisini yazdirin.
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Lutfen yasinizi giriniz");
+
+        double yas= scan.nextDouble();
+
+        if (yas >= 65){
+            System.out.println("Emekli olabilirsin");
+        } else {
+
+            System.out.println("Emekli olabilmek icin daha " +(65-yas)+ " yil calismalisin");
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
+
